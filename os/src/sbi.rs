008269133b85b3e348857207646a2585a2275aa3
Jeremy <Jeremy_Merle@outlook.com>
2022-03-03 22:33:18 +0800

    修改-增加注释

--- os/src/sbi.rs
+++ os/src/sbi.rs
@@ -1,4 +1,12 @@
-// os/src/sbi/rs
+/// # SBI相关模块
+/// `os/src/sbi.rs`
+/// ## 实现功能
+/// ```
+/// pub fn console_putchar(c: usize)
+/// pub fn shutdown()
+/// ```
+//
+
 use core::arch::asm;
 
 //定义 RustSBI 支持的服务类型常量
@@ -13,10 +21,11 @@ const SBI_REMOTE_SFENCE_VMA:        usize = 6;
 const SBI_REMOTE_SFENCE_VMA_ASID:   usize = 7;
 const SBI_SHUTDOWN:                 usize = 8;
 
+/// ### SBI调用
+/// - `which` 表示请求 RustSBI 的服务的类型
+/// - `arg0` ~ `arg2` 表示传递给 RustSBI 的 3 个参数
+/// - `RustSBI` 在将请求处理完毕后，会给内核一个返回值，这个返回值也会被 sbi_call 函数返回
 #[inline(always)]
-// which 表示请求 RustSBI 的服务的类型
-// arg0 ~ arg2 表示传递给 RustSBI 的 3 个参数
-// RustSBI 在将请求处理完毕后，会给内核一个返回值，这个返回值也会被 sbi_call 函数返回
 fn sbi_call(whitch: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
     let mut ret;
     unsafe{
@@ -31,10 +40,17 @@ fn sbi_call(whitch: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
     ret
 }
 
+/// ### 向终端输出一个字符
+/// - 采用`sbi_call()`实现
+/// - 参数
+///     - `c`: 待输出的字符
 pub fn console_putchar(c: usize) {
     sbi_call(SBI_CONSOLE_PUTCHAR, c, 0, 0);
 }
 
+/// ### 调用SBI_Call关机
+/// - 采用sbi_call实现
+/// - 若关机失败则引发异常
 pub fn shutdown() -> ! {
     sbi_call(SBI_SHUTDOWN,0 ,0 ,0);
     panic!("It shuld shudtdown!");
