e2d46cac0afc0e3d93b71de370904768606014d1
Jeremy <Jeremy_Merle@outlook.com>
2022-03-03 14:50:02 +0800

    修改-错误处理函数

--- user/src/lang_items.rs
+++ user/src/lang_items.rs
@@ -1,18 +1,20 @@
 // user/src/lang_items.rs
-use crate::sbi::shutdown;
 use core::panic::PanicInfo;
 
+
+/// 打印处出错信息后陷入死循环
 #[panic_handler]    //通知编译器用panic函数来对接 panic! 宏
-fn panic(info:&PanicInfo) -> ! {
-    if let Some(location) = info.location() {
+fn panic(panic_info:&PanicInfo) -> ! {
+    let err = panic_info.message().unwrap();
+    if let Some(location) = panic_info.location() {
         println!(
-            "Panicked at {}:{} {}",
+            "Panicked at {}:{}, {}",
             location.file(),
             location.line(),
-            info.message().unwrap()
+            err
         );
     } else {
-        println!("Panicked: {}", info.message().unwrap());
+        println!("Panicked: {}", err);
     }
-    shutdown()
+    loop {}
 }
