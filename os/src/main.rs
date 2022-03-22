8b808b34be0360e044df14a1a6b80d076516ff31
Jeremy <Jeremy_Merle@outlook.com>
2022-03-22 16:23:26 +0800

    新增-用户程序

--- os/src/main.rs
+++ os/src/main.rs
@@ -41,13 +41,14 @@ pub fn rust_main() -> ! {
     clear_bss();
     println!("[kernel] Hello, world!");
     mm::init();
-    println!("[kernel] back to world!");
     mm::remap_test();
+    task::add_initproc();
+    println!("[kernel] add initproc!");
     trap::init();
-    //trap::enable_interrupt();
     trap::enable_timer_interrupt();
     timer::set_next_trigger();
-    task::run_first_task();
+    loader::list_apps();
+    task::run_tasks();
     panic!("Unreachable in rust_main!");
 }
 
