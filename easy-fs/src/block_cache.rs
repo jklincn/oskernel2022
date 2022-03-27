7edf9e3d3bba31bf10790a65a1496ba95e0d02d3
jklinCN <985765408@qq.com>
2022-03-26 21:17:01 -0700

    块接口、块缓存注释完毕

--- easy-fs/src/block_cache.rs
+++ easy-fs/src/block_cache.rs
@@ -5,16 +5,17 @@ use lazy_static::*;
 use spin::Mutex;
 
 pub struct BlockCache {
-    cache: [u8; BLOCK_SZ],
-    block_id: usize,
-    block_device: Arc<dyn BlockDevice>,
-    modified: bool,
+    cache: [u8; BLOCK_SZ],              // 512 字节数组，表示位于内存中的缓冲区
+    block_id: usize,                    // 记录这个块缓存来自于磁盘中的块的编号
+    block_device: Arc<dyn BlockDevice>, // 底层块设备的引用，可通过它进行块读写
+    modified: bool,                     //记录这个块从磁盘载入内存缓存之后，它有没有被修改过
 }
 
 impl BlockCache {
-    /// Load a new BlockCache from disk.
+    /// 从磁盘上加载一个块缓存
     pub fn new(block_id: usize, block_device: Arc<dyn BlockDevice>) -> Self {
         let mut cache = [0u8; BLOCK_SZ];
+        // 将一个块上的数据从磁盘读到内存中的一个缓冲区中
         block_device.read_block(block_id, &mut cache);
         Self {
             cache,
@@ -24,10 +25,12 @@ impl BlockCache {
         }
     }
 
+    /// 得到缓冲区中指定偏移量 offset 的字节地址
     fn addr_of_offset(&self, offset: usize) -> usize {
         &self.cache[offset] as *const _ as usize
     }
 
+    /// 获取缓冲区中的位于偏移量 offset 的一个类型为 T 的磁盘上数据结构的不可变引用
     pub fn get_ref<T>(&self, offset: usize) -> &T
     where
         T: Sized,
@@ -38,25 +41,29 @@ impl BlockCache {
         unsafe { &*(addr as *const T) }
     }
 
+    /// 获取缓冲区中的位于偏移量 offset 的一个类型为 T 的磁盘上数据结构的可变引用
     pub fn get_mut<T>(&mut self, offset: usize) -> &mut T
     where
         T: Sized,
     {
         let type_size = core::mem::size_of::<T>();
         assert!(offset + type_size <= BLOCK_SZ);
-        self.modified = true;
+        self.modified = true; // 标记为已修改
         let addr = self.addr_of_offset(offset);
         unsafe { &mut *(addr as *mut T) }
     }
 
+    /// 获取不可变引用后执行指定函数
     pub fn read<T, V>(&self, offset: usize, f: impl FnOnce(&T) -> V) -> V {
         f(self.get_ref(offset))
     }
 
+    /// 获取可变引用后执行指定函数
     pub fn modify<T, V>(&mut self, offset: usize, f: impl FnOnce(&mut T) -> V) -> V {
         f(self.get_mut(offset))
     }
 
+    /// 将缓冲区中的内容写回到磁盘块中
     pub fn sync(&mut self) {
         if self.modified {
             self.modified = false;
@@ -71,10 +78,12 @@ impl Drop for BlockCache {
     }
 }
 
+/// 同时只能驻留 16 个磁盘块的缓冲区
 const BLOCK_CACHE_SIZE: usize = 16;
 
+/// 块缓存管理器
 pub struct BlockCacheManager {
-    queue: VecDeque<(usize, Arc<Mutex<BlockCache>>)>,
+    queue: VecDeque<(usize, Arc<Mutex<BlockCache>>)>,   // 块编号和块缓存的二元组，Arc<Mutex<>>可以同时提供共享引用和互斥访问
 }
 
 impl BlockCacheManager {
@@ -89,12 +98,13 @@ impl BlockCacheManager {
         block_id: usize,
         block_device: Arc<dyn BlockDevice>,
     ) -> Arc<Mutex<BlockCache>> {
+        // 先在队列中寻找，若找到则将块缓存的引用复制一份并返回
         if let Some(pair) = self.queue.iter().find(|pair| pair.0 == block_id) {
             Arc::clone(&pair.1)
         } else {
-            // substitute
+            // 判断块缓存数量是否到达上线
             if self.queue.len() == BLOCK_CACHE_SIZE {
-                // from front to tail
+                // FIFO 替换，找强引用计数为1的替换出去
                 if let Some((idx, _)) = self
                     .queue
                     .iter()
@@ -103,25 +113,29 @@ impl BlockCacheManager {
                 {
                     self.queue.drain(idx..=idx);
                 } else {
+                    // 队列已满且其中所有的块缓存都正在使用的情形
                     panic!("Run out of BlockCache!");
                 }
             }
-            // load block into mem and push back
+            // 创建新的块缓存（会触发 read_block 进行块读取）
             let block_cache = Arc::new(Mutex::new(BlockCache::new(
                 block_id,
                 Arc::clone(&block_device),
             )));
+            // 加入到队尾，最后返回
             self.queue.push_back((block_id, Arc::clone(&block_cache)));
             block_cache
         }
     }
 }
 
+/// 定义块缓存全局管理器
 lazy_static! {
     pub static ref BLOCK_CACHE_MANAGER: Mutex<BlockCacheManager> =
         Mutex::new(BlockCacheManager::new());
 }
 
+/// 公开接口：获取块缓存
 pub fn get_block_cache(
     block_id: usize,
     block_device: Arc<dyn BlockDevice>,
@@ -131,6 +145,7 @@ pub fn get_block_cache(
         .get_block_cache(block_id, block_device)
 }
 
+/// 公开接口：把所有块缓存中的内容同步到磁盘中
 pub fn block_cache_sync_all() {
     let manager = BLOCK_CACHE_MANAGER.lock();
     for (_, cache) in manager.queue.iter() {
