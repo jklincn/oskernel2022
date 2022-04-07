ae564649f67ba1d625bf589a0bfe4b245007b385
Jeremy <Jeremy_Merle@outlook.com>
2022-04-07 09:20:06 +0800

    新增-内核文件系统接口

---
+++ os/src/fs/mod.rs
@@ -0,0 +1,20 @@
+/// # 内核文件系统接口
+/// `os/src/fs/mod.rs`
+//
+
+mod inode;  // 内核索引节点层
+mod stdio;  // 标准输入输出接口
+
+use crate::mm::UserBuffer;
+
+pub trait File: Send + Sync {
+    fn readable(&self) -> bool;
+    fn writable(&self) -> bool;
+    /// read 指的是从文件中读取数据放到缓冲区中，最多将缓冲区填满，并返回实际读取的字节数
+    fn read(&self, buf: UserBuffer) -> usize;
+    /// 将缓冲区中的数据写入文件，最多将缓冲区中的数据全部写入，并返回直接写入的字节数
+    fn write(&self, buf: UserBuffer) -> usize;
+}
+
+pub use inode::{list_apps, open_file, OSInode, OpenFlags};
+pub use stdio::{Stdin, Stdout};
