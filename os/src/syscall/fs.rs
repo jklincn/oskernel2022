7b999d0f2bbdea15e1772c37b4e5dad316171255
Jeremy <Jeremy_Merle@outlook.com>
2022-04-07 10:52:31 +0800

    修改-文件读写模块 sys_read/write 函数

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -4,15 +4,14 @@
 /// ```
 /// pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize
 /// pub fn sys_read(fd: usize, buf: *const u8, len: usize) -> isize
+/// pub fn sys_open(path: *const u8, flags: u32) -> isize
+/// pub fn sys_close(fd: usize) -> isize
 /// ```
 //
 
-use crate::mm::translated_byte_buffer;
-use crate::sbi::console_getchar;
-use crate::task::{current_user_token, suspend_current_and_run_next};
-
-const FD_STDIN: usize = 0;
-const FD_STDOUT: usize = 1;
+use crate::fs::{open_file, OpenFlags};
+use crate::mm::{translated_byte_buffer, translated_str, UserBuffer};
+use crate::task::{current_task, current_user_token};
 
 /// ### 写文件函数
 /// - `fd` 表示待写入文件的文件描述符；
@@ -20,48 +19,46 @@ const FD_STDOUT: usize = 1;
 /// - `len` 表示内存中缓冲区的长度。
 /// - 返回值：成功写入的长度。
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
-    match fd {
-        FD_STDOUT => {
-            let buffers = translated_byte_buffer(current_user_token(), buf, len);
-            for buffer in buffers {
-                print!("{}", core::str::from_utf8(buffer).unwrap());
-            }
-            len as isize
+    let token = current_user_token();
+    let task = current_task().unwrap();
+    let inner = task.inner_exclusive_access();
+    if fd >= inner.fd_table.len() {
+        return -1;
+    }
+    if let Some(file) = &inner.fd_table[fd] {
+        if !file.writable() {
+            return -1;
         }
-        _ => {
-            panic!("Unsupported fd in sys_write!");
-        }
+        let file = file.clone();
+        // release current task TCB manually to avoid multi-borrow
+        drop(inner);
+        file.write(UserBuffer::new(translated_byte_buffer(token, buf, len))) as isize
+    } else {
+        -1
     }
 }
 
 /// ### 读文件函数
 /// - `fd` 表示待读取文件的文件描述符；
 /// - `buf` 表示内存中缓冲区的起始地址；
-/// - `len` 表示读取字符个数-只能为 1 。
+/// - `len` 表示读取字符个数。
 /// - 返回值：读出的字符。
 pub fn sys_read(fd: usize, buf: *const u8, len: usize) -> isize {
-    match fd {
-        FD_STDIN => {
-            assert_eq!(len, 1, "Only support len = 1 in sys_read!");
-            let mut c: usize;
-            loop {  // 从终端读出一个字符，如果返回 0 则说明没读到就释放CPU，等待输入
-                c = console_getchar();
-                if c == 0 {
-                    suspend_current_and_run_next();
-                    continue;
-                } else {
-                    break;
-                }
-            }
-            let ch = c as u8;   // 将读出的字符写入 buf
-            let mut buffers = translated_byte_buffer(current_user_token(), buf, len);
-            unsafe {
-                buffers[0].as_mut_ptr().write_volatile(ch);
-            }
-            1
+    let token = current_user_token();
+    let task = current_task().unwrap();
+    let inner = task.inner_exclusive_access();
+    if fd >= inner.fd_table.len() {
+        return -1;
+    }
+    if let Some(file) = &inner.fd_table[fd] {
+        let file = file.clone();
+        if !file.readable() {
+            return -1;
         }
-        _ => {
-            panic!("Unsupported fd in sys_read!");
-        }
+        // release current task TCB manually to avoid multi-borrow
+        drop(inner);
+        file.read(UserBuffer::new(translated_byte_buffer(token, buf, len))) as isize
+    } else {
+        -1
     }
 }
