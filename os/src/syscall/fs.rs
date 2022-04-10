0e12ad5d2ce06ec45fa15fd778918b221724af1b
Jeremy <Jeremy_Merle@outlook.com>
2022-04-10 21:55:03 +0800

    新增-创建管道的内核系统调用实现

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -9,9 +9,10 @@
 /// ```
 //
 
-use crate::fs::{open_file, OpenFlags};
-use crate::mm::{translated_byte_buffer, translated_str, UserBuffer};
+use crate::fs::{make_pipe, open_file, OpenFlags};
+use crate::mm::{translated_byte_buffer, translated_refmut, translated_str, UserBuffer};
 use crate::task::{current_task, current_user_token};
+use alloc::sync::Arc;
 
 /// ### 写文件函数
 /// - `fd` 表示待写入文件的文件描述符；
@@ -98,5 +99,24 @@ pub fn sys_close(fd: usize) -> isize {
         return -1;
     }
     inner.fd_table[fd].take();
+    0
+}
+
+/// ### 为当前进程打开一个管道。
+/// - `pipe` 表示应用地址空间中的一个长度为 `2` 的 `usize` 数组的起始地址，
+/// 内核需要按顺序将管道读端和写端的文件描述符写入到数组中。
+/// - 返回值：如果出现了错误则返回 -1，否则返回 0 。可能的错误原因是：传入的地址不合法。
+/// - syscall ID：59
+pub fn sys_pipe(pipe: *mut usize) -> isize {
+    let task = current_task().unwrap();
+    let token = current_user_token();
+    let mut inner = task.inner_exclusive_access();
+    let (pipe_read, pipe_write) = make_pipe();
+    let read_fd = inner.alloc_fd();
+    inner.fd_table[read_fd] = Some(pipe_read);
+    let write_fd = inner.alloc_fd();
+    inner.fd_table[write_fd] = Some(pipe_write);
+    *translated_refmut(token, pipe) = read_fd;
+    *translated_refmut(token, unsafe { pipe.add(1) }) = write_fd;
     0
 }
--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -4,6 +4,7 @@
 /// ```
 /// const SYSCALL_OPEN:     usize = 56;
 /// const SYSCALL_CLOSE:    usize = 57;
+/// const SYSCALL_PIPE:     usize = 59;
 /// const SYSCALL_READ:     usize = 63;
 /// const SYSCALL_WRITE:    usize = 64;
 /// const SYSCALL_EXIT:     usize = 93;
@@ -19,6 +20,7 @@
 
 const SYSCALL_OPEN:     usize = 56;
 const SYSCALL_CLOSE:    usize = 57;
+const SYSCALL_PIPE:     usize = 59;
 const SYSCALL_READ:     usize = 63;
 const SYSCALL_WRITE:    usize = 64;
 const SYSCALL_EXIT:     usize = 93;
@@ -40,6 +42,7 @@ pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
     match syscall_id {
         SYSCALL_OPEN =>     sys_open(args[0] as *const u8, args[1] as u32),
         SYSCALL_CLOSE =>    sys_close(args[0]),
+        SYSCALL_PIPE =>     sys_pipe(args[0] as *mut usize),
         SYSCALL_READ =>     sys_read(args[0], args[1] as *const u8, args[2]),
         SYSCALL_WRITE =>    sys_write(args[0], args[1] as *const u8, args[2]),
         SYSCALL_EXIT =>     sys_exit(args[0] as i32),
