47ba8fa4e344adcba1c831f426ab044742010dba
Jeremy <Jeremy_Merle@outlook.com>
2022-04-21 11:04:34 +0000

    新增-系统调用sys_uname

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -11,6 +11,7 @@
 /// const SYSCALL_EXIT:     usize = 93;
 /// const SYSCALL_YIELD:    usize = 124;
 /// const SYSCALL_KILL:     usize = 129;
+/// const SYSCALL_UNAME:    usize = 160;
 /// const SYSCALL_GET_TIME: usize = 169;
 /// const SYSCALL_GETPID:   usize = 172;
 /// const SYSCALL_FORK:     usize = 220;
@@ -29,6 +30,7 @@ const SYSCALL_WRITE:    usize = 64;
 const SYSCALL_EXIT:     usize = 93;
 const SYSCALL_YIELD:    usize = 124;
 const SYSCALL_KILL:     usize = 129;
+const SYSCALL_UNAME:    usize = 160;
 const SYSCALL_GET_TIME: usize = 169;
 const SYSCALL_GETPID:   usize = 172;
 const SYSCALL_FORK:     usize = 220;
@@ -53,6 +55,7 @@ pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
         SYSCALL_EXIT =>     sys_exit(args[0] as i32),
         SYSCALL_YIELD =>    sys_yield(),
         SYSCALL_KILL =>     sys_kill(args[0], args[1] as u32),
+        SYSCALL_UNAME =>    sys_uname(args[0] as *const u8),
         SYSCALL_GET_TIME => sys_get_time(),
         SYSCALL_GETPID =>   sys_getpid(),
         SYSCALL_FORK =>     sys_fork(),
