9d46b77e786d1dae5223ce1ab771a74ecace567e
Jeremy <Jeremy_Merle@outlook.com>
2022-04-25 01:36:05 +0000

    修改系统调用，wait_pid实现放权等待

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -16,7 +16,7 @@ use crate::fs::{open_file, OpenFlags};
 use crate::mm::{translated_ref, translated_refmut, translated_str};
 use crate::task::{
     add_task, current_task, current_user_token, exit_current_and_run_next, pid2task,
-    suspend_current_and_run_next, SignalFlags,current_trap_cx,
+    suspend_current_and_run_next, SignalFlags,
 };
 use crate::timer::get_time_ms;
 use alloc::string::String;
@@ -115,7 +115,7 @@ pub fn sys_exec(path: *const u8, mut args: *const usize) -> isize {
 pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
     let task = current_task().unwrap();
     // ---- access current TCB exclusively
-    let mut inner = task.inner_exclusive_access();
+    let inner = task.inner_exclusive_access();
 
     // 根据pid参数查找有没有符合要求的进程
     if !inner
@@ -126,39 +126,40 @@ pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
         return -1;
         // ---- release current PCB
     }
+    drop(inner);
 
-    // 查找所有符合PID要求的处于僵尸状态的进程，如果有的话还需要同时找出它在当前进程控制块子进程向量中的下标
-    let pair = inner.children.iter().enumerate().find(|(_, p)| {
-        // ++++ temporarily access child PCB lock exclusively
-        p.inner_exclusive_access().is_zombie() && (pid == -1 || pid as usize == p.getpid())
-        // ++++ release child PCB
-    });
-
-    if let Some((idx, _)) = pair {
-        // 将子进程从向量中移除并置于当前上下文中
-        let child = inner.children.remove(idx);
-        // 确认这是对于该子进程控制块的唯一一次强引用，即它不会出现在某个进程的子进程向量中，
-        // 更不会出现在处理器监控器或者任务管理器中。当它所在的代码块结束，这次引用变量的生命周期结束，
-        // 将导致该子进程进程控制块的引用计数变为 0 ，彻底回收掉它占用的所有资源，
-        // 包括：内核栈和它的 PID 还有它的应用地址空间存放页表的那些物理页帧等等
-        assert_eq!(Arc::strong_count(&child), 1);
-        // 收集的子进程信息返回
-        let found_pid = child.getpid();
-        // ++++ temporarily access child TCB exclusively
-        let exit_code = child.inner_exclusive_access().exit_code;
-        // ++++ release child PCB
-        // 将子进程的退出码写入到当前进程的应用地址空间中
-        *translated_refmut(inner.memory_set.token(), exit_code_ptr) = exit_code;
-        found_pid as isize
-    } else {
-        // 如果找不到的话则放权等待
-        drop(inner);        // 手动释放 TaskControlBlock 全局可变部分
-        let mut cx = current_trap_cx();
-        cx.sepc -= 4;
-        suspend_current_and_run_next();
-        -2
+    loop{
+        let mut inner = task.inner_exclusive_access();
+        // 查找所有符合PID要求的处于僵尸状态的进程，如果有的话还需要同时找出它在当前进程控制块子进程向量中的下标
+        let pair = inner.children.iter().enumerate().find(|(_, p)| {
+            // ++++ temporarily access child PCB lock exclusively
+            p.inner_exclusive_access().is_zombie() && (pid == -1 || pid as usize == p.getpid())
+            // ++++ release child PCB
+        });
+        
+        if let Some((idx, _)) = pair {
+            // 将子进程从向量中移除并置于当前上下文中
+            let child = inner.children.remove(idx);
+            // 确认这是对于该子进程控制块的唯一一次强引用，即它不会出现在某个进程的子进程向量中，
+            // 更不会出现在处理器监控器或者任务管理器中。当它所在的代码块结束，这次引用变量的生命周期结束，
+            // 将导致该子进程进程控制块的引用计数变为 0 ，彻底回收掉它占用的所有资源，
+            // 包括：内核栈和它的 PID 还有它的应用地址空间存放页表的那些物理页帧等等
+            assert_eq!(Arc::strong_count(&child), 1);
+            // 收集的子进程信息返回
+            let found_pid = child.getpid();
+            // ++++ temporarily access child TCB exclusively
+            let exit_code = child.inner_exclusive_access().exit_code;
+            // ++++ release child PCB
+            // 将子进程的退出码写入到当前进程的应用地址空间中
+            *translated_refmut(inner.memory_set.token(), exit_code_ptr) = exit_code;
+            return found_pid as isize;
+        } else {
+            // 如果找不到的话则放权等待
+            drop(inner);        // 手动释放 TaskControlBlock 全局可变部分
+            suspend_current_and_run_next();
+        }
+        // ---- release current PCB lock automatically
     }
-    // ---- release current PCB lock automatically
 }
 
 pub fn sys_kill(pid: usize, signal: u32) -> isize {
