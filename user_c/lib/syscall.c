0a9a6d2f7ab62a40e4a61eb12007642c215a4517
Jeremy <Jeremy_Merle@outlook.com>
2022-04-25 01:13:53 +0000

    新增-用户态系统调用 sys_yield

--- user_c/lib/syscall.c
+++ user_c/lib/syscall.c
@@ -9,23 +9,21 @@ ssize_t write(int fd, const void* buf, size_t len) {
 ssize_t read(int fd, void* buf, size_t len) {
     return syscall(SYSCALL_READ, fd, buf, len);
 }
-
 void exit(int exit_code) {
     syscall(SYSCALL_EXIT, (int64)exit_code, 0, 0);
 }
-
 int uname(void* buf){
     return syscall(SYSCALL_UNAME, buf);
 }
-
 pid_t fork(void){
     return syscall(SYSCALL_FORK, SIGCHLD, 0);
 }
-
 int waitpid(int pid, int* code, int options){
     return syscall(SYSCALL_WAITPID, pid, code, options, 0);
 }
-
 int wait(int* code){
     return waitpid((int)-1, code, 0);
+}
+int sched_yield(void){
+    return syscall(SYSCALL_YIELD);
 }
