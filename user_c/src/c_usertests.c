8f45e016788d6c2fb4efbe19d3e14d5c78af5de5
Jeremy <Jeremy_Merle@outlook.com>
2022-05-07 03:01:30 +0000

    新增-测试程序 gettimeofday

--- user_c/src/c_usertests.c
+++ user_c/src/c_usertests.c
@@ -2,9 +2,9 @@
 #include "unistd.h"
 #include "stdlib.h"
 
-#define PROG_NUM 7
+#define PROG_NUM 8
 
-char* prog_name[PROG_NUM] = { "c_uname", "c_fork", "c_exit", "c_exec", "c_getpid" , "c_sleep", "c_getppid" };
+char* prog_name[PROG_NUM] = { "c_uname", "c_fork", "c_exit", "c_exec", "c_getpid" , "c_sleep", "c_gettimeofday", "c_getppid" };
 
 int main() {
     for (int t = 0; t < PROG_NUM; t++) {
