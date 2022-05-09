f37f1c5e24b2c2aa391fc35fcd717f3bdd01468e
jklinCN <985765408@qq.com>
2022-05-08 22:41:25 -0700

    在初始程序中删去颜色打印

--- user_c/src/c_usertests.c
+++ user_c/src/c_usertests.c
@@ -4,7 +4,7 @@
 
 #define PROG_NUM 11
 
-char* prog_name[] = { "uname", "fork", "exit", "execve", "getpid" , "sleep", "gettimeofday", "dup", "times", "user_shell", "c_getppid" };
+char* prog_name[] = { "fork", "exit", "execve", "getpid" , "sleep", "gettimeofday", "dup", "times", "user_shell", "c_getppid","uname"  };
 
 int main() {
     for (int t = 0; t < PROG_NUM; t++) {
@@ -18,13 +18,10 @@ int main() {
         else {          // 父进程
             child_return = -1;
             waitpid(npid, &child_return, 0);
-            printf(COLOR_YELLOW"%s\t"COLOR_NONE, prog_name[t]);
             if (child_return != 0) {
-                printf(COLOR_LIGHT_RED"TEST ERROR:%s return code:%d\n"COLOR_NONE, prog_name[t], child_return);
                 return -t;
             }
             else{
-                printf(COLOR_LIGHT_GREEN"OK\n"COLOR_NONE);
             }
         }
     }
