822e4e620f9bb0fcce14bc12a53cb992392c8712
jklinCN <985765408@qq.com>
2022-08-01 06:32:07 -0700

    修改initprog以支持静态和动态共同测试

--- user_c/src/usertests_submit.c
+++ user_c/src/usertests_submit.c
@@ -2,43 +2,33 @@
 #include "unistd.h"
 #include "stdlib.h"
 
-// 测试动态链接程序
-// #define DYNAMIC
+// 测试所有程序
+#define TEST_ALL
 
-// 测试单一程序
-// #define TEST_ONE
-
+#ifndef TEST_ALL
+char *argv[] = {"./runtest.exe", "-w", "entry-dynamic.exe", "prog_name", 0};
+#else
+int offset = 0;
 #define PROG_NAME_MAX_LENGTH 40
 #define BUFFER_SIZE 6500
 
-#ifndef DYNAMIC
-#define PROG_NUM 109
-#else
-#define PROG_NUM 111
-#endif
+#define STATIC_PROG_NUM 109
+#define DYNAMIC_PROG_NUM 111
 
 char prog_name[PROG_NAME_MAX_LENGTH];
 char buf[BUFFER_SIZE];
 
-// 设置调试程序，prog_name 只有在测试全部程序下使用
-#ifndef DYNAMIC
-    char *argv[] = {"./runtest.exe", "-w", "entry-static.exe", prog_name, 0};
-#else
-    char *argv[] = {"./runtest.exe", "-w", "entry-dynamic.exe", prog_name, 0};
-#endif
+char *static_argv[] = {"./runtest.exe", "-w", "entry-static.exe", prog_name, 0};
+char *dynamic_argv[] = {"./runtest.exe", "-w", "entry-dynamic.exe", prog_name, 0};
 
-#ifndef TEST_ONE
-int offset = 0;
-#ifndef DYNAMIC
 // 静态跳过程序
-#define PROG_PASS_LENGTH 21
-char *prog_pass[] = {
+#define STATIC_PROG_PASS_LENGTH 20
+char *static_prog_pass[] = {
                      "pthread_cancel_points",
                      "pthread_cancel",
                      "pthread_cond",
                      "pthread_tsd",
                      "fflush_exit",
-                     "daemon_failure",
                      "pthread_robust_detach",
                      "pthread_cancel_sem_wait",
                      "pthread_cond_smasher",
@@ -56,15 +46,14 @@ char *prog_pass[] = {
                      "lseek_large",
                      "setvbuf_unget",
                      };
-#else
+
 // 动态跳过程序
-#define PROG_PASS_LENGTH 17
-char *prog_pass[] = {
+#define DYNAMIC_PROG_PASS_LENGTH 24
+char *dynamic_prog_pass[] = {
                      "pthread_cancel_points",
                      "pthread_cancel",
                      "pthread_cond",
                      "pthread_tsd",
-                     "daemon_failure",
                      "fflush_exit",
                      "pthread_robust_detach",
                      "pthread_cond_smasher",
@@ -78,9 +67,15 @@ char *prog_pass[] = {
                      "sem_init",
                      "socket",
                      "sscanf_long",
-
+                     "stat",
+                     "tls_init",
+                     "tls_local_exec",
+                     "ungetc",
+                     "utime",
+                     "lseek_large",
+                     "setvbuf_unget",
+                     "tls_get_new_dtv",
                      };
-#endif
 
 void read_test_name()
 {
@@ -109,10 +104,18 @@ int mystrcmp(const char *s1, const char *s2)
     return *s1 - *s2;
 }
 
-int ifpass()
+int ifpass_static()
 {
-    for (int i = 0; i < PROG_PASS_LENGTH; i++)
-        if (!mystrcmp(prog_pass[i], prog_name))
+    for (int i = 0; i < STATIC_PROG_PASS_LENGTH; i++)
+        if (!mystrcmp(static_prog_pass[i], prog_name))
+            return 1;
+    return 0;
+}
+
+int ifpass_dynamic()
+{
+    for (int i = 0; i < DYNAMIC_PROG_PASS_LENGTH; i++)
+        if (!mystrcmp(dynamic_prog_pass[i], prog_name))
             return 1;
     return 0;
 }
@@ -120,40 +123,7 @@ int ifpass()
 
 int main()
 {
-#ifndef TEST_ONE
-    #ifndef DYNAMIC
-    // run all tests
-    int fd = open("./run-static.sh", 0);
-    #else
-    int fd = open("./run-dynamic.sh", 0);
-    #endif
-    read(fd, buf, BUFFER_SIZE);
-
-    for (int row = 0; row < PROG_NUM; row++)
-    {
-        read_test_name();
-
-        // pass some programs
-        if (ifpass())
-            continue;
-
-        int npid = fork();
-        assert(npid >= 0);
-        int child_return;
-        if (npid == 0)
-        {
-            // child
-            execve("./runtest.exe", argv, NULL);
-        }
-        else
-        {
-            // parent
-            child_return = -1;
-            waitpid(npid, &child_return, 0);
-        }
-    }
-    return 0;
-#else
+#ifndef TEST_ALL
     //test only one program
     int npid = fork();
     assert(npid >= 0);
@@ -169,6 +139,64 @@ int main()
         waitpid(npid, &child_return, 0);
     }
 
+    return 0;
+#else
+    // 运行静态测试程序
+    int fd = open("./run-static.sh", 0);
+    read(fd, buf, BUFFER_SIZE);
+
+    for (int row = 0; row < STATIC_PROG_NUM; row++)
+    {
+        read_test_name();
+
+        // pass some programs
+        if (ifpass_static())
+            continue;
+
+        int npid = fork();
+        assert(npid >= 0);
+        int child_return;
+        if (npid == 0)
+        {
+            // child
+            execve("./runtest.exe", static_argv, NULL);
+        }
+        else
+        {
+            // parent
+            child_return = -1;
+            waitpid(npid, &child_return, 0);
+        }
+    }
+
+    // 运行动态测试程序
+    fd = open("./run-dynamic.sh", 0);
+    offset = 0;
+    read(fd, buf, BUFFER_SIZE);
+
+    for (int row = 0; row < DYNAMIC_PROG_NUM; row++)
+    {
+        read_test_name();
+
+        // pass some programs
+        if (ifpass_dynamic())
+            continue;
+
+        int npid = fork();
+        assert(npid >= 0);
+        int child_return;
+        if (npid == 0)
+        {
+            // child
+            execve("./runtest.exe", dynamic_argv, NULL);
+        }
+        else
+        {
+            // parent
+            child_return = -1;
+            waitpid(npid, &child_return, 0);
+        }
+    }
     return 0;
 #endif
 }
