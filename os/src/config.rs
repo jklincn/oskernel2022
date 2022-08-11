ca54bb7ac11fe32060d4f82c5e28b62e6a6bce8e
jklinCN <985765408@qq.com>
2022-08-10 23:03:01 -0700

    重写initproc，通过lua和busybox的回归测试(调大内存后)

--- os/src/config.rs
+++ os/src/config.rs
@@ -7,8 +7,7 @@
 pub const USER_STACK_SIZE:      usize = 4096 * 4;
 pub const KERNEL_STACK_SIZE:    usize = 4096 * 4;
 
-
-pub const KERNEL_HEAP_SIZE:    usize = 4096 * 32; // 128K
+pub const KERNEL_HEAP_SIZE:    usize = 4096 * 36; // 144KB
 pub const USER_HEAP_SIZE: usize = 4096 * 10;
 
 /// 指定内存终止物理地址，内存大小为6MiB（左闭右开）(8M有大坑，会随机卡死)
