7078e56512966627ba9a57c6102c8d31d60f9d88
jklinCN <985765408@qq.com>
2022-08-15 05:19:43 -0700

    再次提交评测

--- user_c/src/usertests_submit.c
+++ user_c/src/usertests_submit.c
@@ -43,7 +43,7 @@ char *argv_lmbench27[] = {"./lmbench_all", "lat_ctx", "-P", "1", "-s", "32", "24
 char *argv_lmbench28[] = {"./lmbench_all", "lat_ctx", "-P", "1", "-s", "32", "32", 0};
 char *argv_lmbench29[] = {"./lmbench_all", "lat_ctx", "-P", "1", "-s", "32", "64", 0};
 char *argv_lmbench30[] = {"./lmbench_all", "lat_ctx", "-P", "1", "-s", "32", "96", 0};
-const int lmbench_test_num = 21;
+const int lmbench_test_num = 20;
 char **argv_lmbench[] =
     {
         argv_lmbench0,
